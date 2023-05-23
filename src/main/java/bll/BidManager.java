02aa2b91824dbf8e3b20bc74df1b9a420fe4f16a
Mathieu-Charron <130163715+Mathieu-Charron@users.noreply.github.com>
2023-05-23 17:36:12 +0200

    feat(BLL): ajout verif pour les items dans la BLL

--- src/main/java/bll/BidManager.java
+++ src/main/java/bll/BidManager.java
@@ -19,6 +19,10 @@ public class BidManager {
 	private ISoldItemDAO soldItemDAO;
 	private static BidManager instance;
 	
+	private static final int MAX_ITEM_NAME_SIZE = 30;
+	private static final int MAX_DESCRIPTION_SIZE = 300;
+
+	
 	private static final String ERROR_BDD = "Une erreur est survenue";
 
 	
@@ -83,18 +87,23 @@ public class BidManager {
 		
 		String errorMessage = "";
 		//verif itemName <=30
+		errorMessage+= checkItemNameSize(itemName);
 		
 		//verif description <=300
+		errorMessage+= checkDescriptionSize(description);
+
 		
 		//verif intialPrice >0
+		errorMessage+= initialPrice > 0 ? "" : "Le prix doit être supérieur à 0";
+		
+		//check is date format
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate startDate = LocalDate.parse(startDateString, formatter);
+		LocalDate endDate = LocalDate.parse(endDateString, formatter);
 		
 		//verif startDate>today && endDate>startDate
 		
 		if(errorMessage.length()>0) throw new BLLException(errorMessage);
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		LocalDate startDate = LocalDate.parse(startDateString, formatter);
-		LocalDate endDate = LocalDate.parse(endDateString, formatter);
 		
 		try {
 			soldItemDAO.insert(new SoldItem(itemName, initialPrice, startDate, endDate, seller, description, new Category(categoryId), new Withdrawal(street, postalCode, city)));
@@ -103,6 +112,28 @@ public class BidManager {
 			throw new BLLException(ERROR_BDD);
 		}
 	}
+	
+	
+	private String checkItemNameSize(String itemName) {
+		
+		if(itemName.length()>MAX_ITEM_NAME_SIZE) return "Le nom de l'article doit contenir maximum " + MAX_ITEM_NAME_SIZE + " charactères";
+		return "";
+	}
+	
+	private String checkDescriptionSize(String description) {
+		
+		if(description.length()>MAX_DESCRIPTION_SIZE) return "La description doit contenir maximum " + MAX_DESCRIPTION_SIZE + " charactères";
+		return "";
+	}
+	
+	private String checkDate(LocalDate startDate,LocalDate endDate) {
+		String errorMessage="";
+		if(startDate.isBefore(LocalDate.now())) {
+			errorMessage+= "La date "
+		}
+		return errorMessage;
+	}
+	
 	
 //	public void insertBid(int itemId, )
 }
